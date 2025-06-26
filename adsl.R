5b9b59cd7e6a6edeea99c187f1cd130f848d7cf8
Caoimhin Ua Buachalla <caoimhin.ua_buachalla@novartis.com>
2025-06-26 07:16:34 -0700

    Pushed from Domino: https://dev.domino.novartis.net/workspace/uabuaca1/CLNP023C1_CLNP023C12303_CSR_1_ANALYSIS_BLINDED?executionId=685d550c13fbb6743f19346f

---
+++ adsl.R
@@ -0,0 +1,5 @@
+load("/mnt/imported/data/CLNP023C1_CLNP023C12303_SDTM_BLINDED/dm.RData")
+
+adsl <- within(dm, USUBJID <- paste(STUDYID, SUBJID, sep="_"))
+
+save(adsl, file="/mnt/data/CLNP023C1_CLNP023C12303_CSR_1_ADAM_BLINDED/adslRData")
